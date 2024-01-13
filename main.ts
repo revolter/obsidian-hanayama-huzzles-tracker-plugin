bfe4f20366bd00194231911fd570a8de6afd59bb
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2024-01-13 14:53:08 +0200

    Added draft boilerplate code

--- main.ts
+++ main.ts
@@ -1,15 +1,55 @@
+import dedent from 'dedent';
+import escapeStringRegExp from 'escape-string-regexp';
 import { Editor, MarkdownView, Plugin } from 'obsidian';
 
 export default class HanayamaHuzzlesTrackerPlugin extends Plugin {
+	private static #startMarker: string = '<!-- Hanayama Huzzles start -->';
+	private static #endMarker: string = '<!-- Hanayama Huzzles end -->';
+
 	async onload() {
 		this.addCommand({
 			id: 'update-list',
 			name: 'Update list',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Update list');
+				const content: string = editor.getValue();
+				const newContent: string = this.#updatedListInContent(content);
+
+				editor.setValue(newContent);
 			}
 		});
 	}
 
 	onunload() {}
+
+	private #updatedListInContent(content: string): string {
+		const escapedStartMarker: string = escapeStringRegExp(HanayamaHuzzlesTrackerPlugin.#startMarker);
+		const escapedEndMarker: string = escapeStringRegExp(HanayamaHuzzlesTrackerPlugin.#endMarker);
+
+		const regex: RegExp = new RegExp(`${escapedStartMarker}(?<list>.*?)${escapedEndMarker}`, 's');
+		const match: [string] = content.match(regex);
+
+		if (match != null && match.groups != null && match.groups.list != null) {
+			const list: string = match.groups.list;
+			const updatedList: string = this.#updatedList(list);
+			const updatedContent: string = content.replace(regex, `${HanayamaHuzzlesTrackerPlugin.#startMarker}${updatedList}${HanayamaHuzzlesTrackerPlugin.#endMarker}`);
+
+			return updatedContent;
+		} else {
+			const list: string = 'updated list';
+			const updatedList: string = this.#updatedList(list);
+
+			return dedent
+				`${content}
+
+				${HanayamaHuzzlesTrackerPlugin.#startMarker}
+
+				${updatedList}
+
+				${HanayamaHuzzlesTrackerPlugin.#endMarker}`;
+		}
+	}
+
+	private #updatedList(list: string): string {
+		return list.toUpperCase();
+	}
 }
